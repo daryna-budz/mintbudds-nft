33ccb2789b460f753792b2f944dd10dbf8eb79fd
Daryna Budz <78912711+daryna-budz@users.noreply.github.com>
2025-04-02 01:03:15 -0400

    Revert "Implemented Homepage (Homepage.jsx component) and added dynamic table…"

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,7 @@ export default {
     extend: {
       fontFamily: {
         spartan: ['League Spartan', 'sans-serif'],
-      },
-      animation: {
-        float1: "float 3s ease-in-out infinite",
-        float2: "float 3s ease-in-out infinite 1s",
-        float3 : "float 3s ease-in-out infinite 2s",
-      },
-      keyframes: {
-        float: {
-          "0%, 100%": { transform: "translateY(0px)" },
-          "50%": { transform: "translateY(-10px)" },
-        },
-      },
+      }  
     },
   },
   plugins: [],
